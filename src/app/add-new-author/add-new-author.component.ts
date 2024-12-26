7726eb9ee80e531ec32a8c6f8016dab84ed8f436
karnan131k <sooriyakumarkarnan@gmail.com>
2024-12-26 15:56:01 +0530

    add author and category and book edit and update functionality with image upload

--- src/app/add-new-author/add-new-author.component.ts
+++ src/app/add-new-author/add-new-author.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookStreamService } from '../book-stream.service';
+import { apiUrl, AuthorRequestDTO } from '../menu.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-new-author',
@@ -9,4 +14,97 @@ import { Component } from '@angular/core';
 })
 export class AddNewAuthorComponent {
 
+  authorForm!: FormGroup;
+  previewImage: string | null = null;
+  imageError: string | null = null;
+  isEditMode = false;
+  authorId!: number;
+  basePath: string = apiUrl;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authorService: BookStreamService,
+    private snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+    this.route.params.subscribe(params => {
+      this.authorId = params['id'];
+      this.isEditMode = !!this.authorId;
+      if (this.isEditMode) {
+        this.loadAuthorData();
+      }
+    });
+  }
+
+  initForm() {
+    this.authorForm = this.fb.group({
+      name: ['', Validators.required],
+      imagePath: ['']
+    });
+  }
+
+  loadAuthorData() {
+    this.authorService.getAuthorDetail(this.authorId).subscribe(response => {
+      const author = response.data;
+      if (author) {
+        this.authorForm.patchValue(author);
+        this.previewImage = author.imagePath;
+      }
+    });
+  }
+
+  onFileSelect(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const folderName = 'authors';
+      this.authorService.uploadImage(file, folderName).subscribe(
+        response => {
+          this.previewImage = response.data!;
+          this.authorForm.get('imagePath')?.setValue(response.data!);
+          this.imageError = null;
+        },
+        error => {
+          this.imageError = 'Failed to upload image. Please try again.';
+        }
+      );
+    }
+  }
+
+  removeImage() {
+    if (this.previewImage) {
+      const folderName = 'authors';
+      const imageName = this.previewImage.split('/').pop()?.split('.')[0];
+      this.authorService.deleteImage(folderName, imageName!).subscribe(() => {
+        this.previewImage = null;
+        this.authorForm.get('imagePath')?.reset();
+      });
+    }
+  }
+
+  onSubmit() {
+    const payload: AuthorRequestDTO = this.authorForm.value;
+    if (this.isEditMode) {
+      this.authorService.updateAuthorDetail(payload, this.authorId).subscribe(() => {
+        this.snackBar.open('Author updated successfully!', 'Close', { duration: 3000 });
+        this.resetForm();
+        
+      });
+    } else {
+      this.authorService.createAuthorDetail(payload).subscribe(() => {
+        this.snackBar.open('Author updated successfully!', 'Close', { duration: 3000 });
+        this.resetForm();
+        
+      });
+    }
+  }
+
+  resetForm() {
+    this.authorForm.reset();
+    this.previewImage = null;
+  }
+  
 }
--- src/app/add-new-category/add-new-category.component.ts
+++ src/app/add-new-category/add-new-category.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookStreamService } from '../book-stream.service';
+import { apiUrl, CategoryRequestDTO } from '../menu.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-new-category',
@@ -9,4 +14,106 @@ import { Component } from '@angular/core';
 })
 export class AddNewCategoryComponent {
 
+  categoryForm!: FormGroup;
+  imagePreview: string | null = null;
+  isEditMode = false;
+  currentCategoryId: number | null = null;
+  basePath: string = apiUrl;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private apiService: BookStreamService,
+    private snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+
+    // Determine mode (add or edit) based on the URL ID
+    this.currentCategoryId = this.route.snapshot.params['id'] ? +this.route.snapshot.params['id'] : null;
+    this.isEditMode = !!this.currentCategoryId;
+
+    if (this.isEditMode && this.currentCategoryId) {
+      this.loadCategoryDetails(this.currentCategoryId);
+    }
+  }
+
+  initForm(): void {
+    this.categoryForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      imagePath: [null, Validators.required],
+    });
+  }
+
+  loadCategoryDetails(id: number): void {
+    this.apiService.getCategoryDetail(id).subscribe((response) => {
+      if (response.status === 'SUCCESS' && response.data) {
+        this.categoryForm.patchValue({
+          name: response.data.name,
+          imagePath: response.data.imagePath,
+        });
+        this.imagePreview = response.data.imagePath;
+      }
+    });
+  }
+
+  onImageUpload(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.apiService.uploadImage(file, 'categories').subscribe((response) => {
+        if (response.status === 'SUCCESS' && response.data) {
+          this.imagePreview = response.data;
+          this.categoryForm.patchValue({ imagePath: response.data });
+        }
+      });
+    }
+  }
+
+  onImageRemove(): void {
+    if (this.imagePreview) {
+      const [folderName, imageNameWithExtension] = this.imagePreview.split('/').slice(-2); // Get folder and file name with extension
+      const imageName = imageNameWithExtension.split('.').slice(0, -1).join('.'); // Remove the extension
+  
+      this.apiService.deleteImage(folderName, imageName).subscribe(() => {
+        this.imagePreview = null;
+        this.categoryForm.patchValue({ imagePath: null });
+      });
+    }
+  }
+  
+
+  onSubmit(): void {
+    if (this.categoryForm.invalid) {
+      return;
+    }
+
+    const payload: CategoryRequestDTO = this.categoryForm.value;
+
+    if (this.isEditMode && this.currentCategoryId) {
+      this.apiService.updateCategoryDetail(payload, this.currentCategoryId).subscribe((response) => {
+        if (response.status === 'SUCCESS') {
+          this.snackBar.open('Category updated successfully!', 'Close', { duration: 3000 });
+          this.resetForm();
+        }
+      });
+    } else {
+      this.apiService.createCategoryDetail(payload).subscribe((response) => {
+        if (response.status === 'SUCCESS') {
+          this.snackBar.open('Category created successfully!', 'Close', { duration: 3000 });
+          this.resetForm();
+        }
+      });
+    }
+  }
+
+  resetForm(): void {
+    this.categoryForm.reset();
+    this.imagePreview = null;
+    if (!this.isEditMode) {
+      this.router.navigate(['/categories']);
+    }
+  }
+  
 }
